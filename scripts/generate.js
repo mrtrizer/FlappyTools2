86261e6445d560bc811b7fa11e113473cfc317f0
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-03-21 02:23:43 +0300

    Improved generation script.

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,46 +1,79 @@
 #!/usr/bin/node
 
+
+function generate(inputData) {
+    let outData = "console.log(\"";
+
+    let state = "text"
+
+    for(var x = 0, c=''; c = inputData.charAt(x); x++){
+        switch (state) {
+            case "text":
+                if (c == "<")
+                    state = "<"
+                else if (c == "\n")
+                    outData += "\\n\\\n";
+                else
+                    outData += c;
+                break;
+            case "js":
+                if (c == "?")
+                    state = "?";
+                else
+                    outData += c;
+                break
+            case "<":
+                if (c == "?") {
+                    outData += "\");\n"
+                    state = "js"
+                }
+                break;
+            case "?":
+                if (c == ">") {
+                    outData += "\nconsole.log(\"";
+                    state = "text";
+                }
+                break;
+        }
+    }
+    outData += "\")";
+    return outData;
+}
+
+function addProperty(object, key, value) {
+    var orig_object = object;
+    var keys = key.split('.');
+
+    for (var i = 0; i < keys.length - 1; i++) {
+        var k = keys[i];
+        if (!object.hasOwnProperty(k)) {
+            object[k] = {};
+            object = object[k];
+        }
+    }
+
+    object[keys.slice(-1)] = value;
+
+    return orig_object;
+}
+
 let args = process.argv.slice(2);
-let inputFile = args[0];
-let outFile = args[1];
+let inputFileName = args[0];
+let outFileName = args[1];
 
 let fs = require('fs');
-let inputData = fs.readFileSync(inputFile, "utf8");
-let outData = "console.log(\"";
+let inputData = fs.readFileSync(inputFileName, "utf8");
 
-let state = "text"
+let rawParams = {"test.first":2, "test.second.third":"test"};
+let outData = generate(inputData);
+let params = {};
 
-for(var x = 0, c=''; c = inputData.charAt(x); x++){
-    switch (state) {
-        case "text":
-            if (c == "<")
-                state = "<"
-            else if (c == "\n")
-                outData += "\\n\\\n";
-            else
-                outData += c;
-            break;
-        case "js":
-            if (c == "?")
-                state = "?";
-            else
-                outData += c;
-            break
-        case "<":
-            if (c == "?") {
-                outData += "\");\n"
-                state = "js"
-            }
-            break;
-        case "?":
-            if (c == ">") {
-                outData += "\nconsole.log(\"";
-                state = "text";
-            }
-            break;
-    }
+for (rawParam in rawParams) {
+    addProperty(params, rawParam, rawParams[rawParam]);
 }
-outData += "\")";
 
-console.log (outData);
-eval (outData);
+console.log(outData);
+
+console.log(params)
+with (params)
+    eval (outData);
