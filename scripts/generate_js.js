1ebe2eb31c47af74d713e05ee6b25315fc8d9f05
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-04-27 21:59:49 +0300

    Changed generator syntax.

--- scripts/generate_js.js
+++ scripts/generate_js.js
@@ -3,74 +3,61 @@
 
 //Script generates javascript code from template file
 
-function genTabs(tabN) {
-    let result = "";
-    while (tabN > 0) {
-        result += "    ";
-        tabN -= 1;
-    }
-    return result;
-}
-
 function generate(inputData) {
     let outData = "\"use strict\"\n" ;
     outData += "output(\"";
 
     let lineStart = false;
-    let tabN = 0;
-    let state = "text"
+    let state = "text";
+    const bracketOpen = "[";
+    const bracketClose = "]";
+    const inlineMarker = "=";
+    const codeBlockMarker = "%";
 
     for(var x = 0, c=''; c = inputData.charAt(x); x++){
         switch (state) {
             case "text":
-                if (c == "<")
-                    state = "<";
-                else if (c == "\n")
-                    outData += "\\n\" + \n" + genTabs(tabN) + "        \"";
-                else if (c == "[") {
-                    state = "[";
-                    outData += "\");\n" + genTabs(tabN) + "output("
-                } else
-                    outData += c;
-                break;
-            case "<":
-                if (c == "?") {
-                    outData += "\");\n"
-                    state = "js_line_start"
-                }
-                break;
-            case "js_line_start":
-                if (c != " ") {
-                    state = "js";
+                if (c == bracketOpen) {
+                    state = "bracket_open";
+                } else if (c == "\n") {
+                    outData += "\\n\" + \n        \"";
                 } else {
-                    break;
-                }
-            case "js":
-                if (c == "\n") {
-                    state = "js_line_start";
-                    outData += "\n";
-                } else if (c == "?")
-                    state = "?";
-                else if (c == "{") {
-                    tabN++;
-                    outData += "{";
-                } else if (c == "}") {
-                    tabN--;
-                    outData += "}";
-                } else
                     outData += c;
-                break
-            case "?":
-                if (c == ">") {
-                    outData += "\n" + genTabs(tabN) + "output(\"";
-                    state = "text";
                 }
                 break;
-            case "[":
+            case "bracket_open":
+                if (c == codeBlockMarker) {
+                    outData += "\");\n"
+                    state = "js"
+                } else if (c == inlineMarker) {
+                    state = "inline";
+                    outData += "\");\n output("
+                } else {
+                    state = "text";
+                    outData += c;
+                }
+                break;
+            case "js":
+                if (c == codeBlockMarker) {
+                    state = "block_end_marker";
+                } else {
+                    outData += c;
+                }
+                break
+            case "block_end_marker":
+                if (c == bracketClose) {
+                    outData += "\n output(\"";
+                    state = "text";
+                } else {
+                    outData += codeBlockMarker;
+                    state = "js"
+                }
+                break;
+            case "inline":
                 if (c == "\n")
                     ;// skip
-                else if (c == "]") {
-                    outData += ");\n" + genTabs(tabN) + "output(\""
+                else if (c == bracketClose) {
+                    outData += ");\n output(\""
                     state = "text";
                 } else {
                     outData += c;
