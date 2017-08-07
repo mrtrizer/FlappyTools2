fca8694a109633d73d9629c0e5afe354a7b26fa1
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-08-07 22:39:54 +0300

    Generator resding and execution.

--- scripts/generate_resources.js
+++ scripts/generate_resources.js
@@ -5,32 +5,33 @@ const utils = require("./utils.js");
 const fs = require("fs");
 
 function findGeneratorsInContext(context) {
+    let generatorScripts = new Array();
     const generatorsDirPath = path.join(context.projectRoot, "generators");
+    console.log(generatorsDirPath);
     if (fs.existsSync(generatorsDirPath)) {
         const content = utils.readDirs(generatorsDirPath);
-        const generatorFiles = content.filter(item => path.extname(item) == js);
-        let generatorScripts = new Object();
+        const generatorFiles = content.filter(item => path.extname(item) == ".js");
+
         for (const i in generatorFiles) {
             const generatorFile = generatorFiles[i];
+            console.log(generatorFile);
             const generatorScript = require(generatorFile);
             generatorScripts.push(generatorScript);
         }
-        return generatorScripts;
     }
+    return generatorScripts;
 }
 
-function getMapOfGenerators(context) {
+function getListOfGenerators(context) {
     const modules = require("./modules.js");
 
-    let generators = new Array();
-
-    generators = generators.concat(findGeneratorsInContext(context));
+    let generators = findGeneratorsInContext(context);
 
     // Iterate all modules in a project
     const allModulesContexts = modules.findAllModules(context);
     for (let i in allModulesContexts) {
         const moduleContext = allModulesContexts[i];
-        generators.concat(findGeneratorsInContext(context));
+        generators = generators.concat(findGeneratorsInContext(moduleContext));
     }
     return generators;
 }
@@ -42,19 +43,29 @@ function getListOfResConfigs(resSrcDir) {
     let resConfigList = new Array();
     for (const i in resConfigPathList) {
         const resConfigPath = resConfigPathList[i];
-        console.log(resConfigPath)
-        try {
-            const configData = fs.readFileSync(resConfigPath, "utf8");
-            const flappyConfig = JSON.parse(configData);
-            resConfigList.push(flappyConfig);
-        } catch (e) {
-            console.log(e.message);
+        if (path.extname(resConfigPath) == ".meta") {
+            console.log(resConfigPath)
+            try {
+                const configData = fs.readFileSync(resConfigPath, "utf8");
+                const flappyConfig = JSON.parse(configData);
+                resConfigList.push(flappyConfig);
+            } catch (e) {
+                console.log(e.message);
+            }
         }
     }
     return resConfigList;
 }
 
-function generateResourcesInContext(context, generatorMap, cacheDir) {
+function generateWithGenerators(config, generatorList, resSrcDir, cacheDir) {
+    for (const i in generatorList) {
+        const generator = generatorList[i];
+        if ((generator.type == config.type) || (generator.type == "*"))
+            generator.generate(config, resSrcDir, cacheDir);
+    }
+}
+
+function generateResourcesInContext(context, generatorList, cacheDir) {
     let cacheSubDir = path.join(cacheDir, context.config.name);
     if (!fs.existsSync(cacheSubDir))
         fs.mkdirSync(cacheSubDir);
@@ -62,40 +73,31 @@ function generateResourcesInContext(context, generatorMap, cacheDir) {
     let resSrcDir = path.join(context.projectRoot, "res_src");
 
     var resConfigList = getListOfResConfigs(resSrcDir);
-
     for (let resConfigN in resConfigList) {
-        generateWithGenerators(resConfigList[resConfigN], generatorMap, resSrcDir, cacheSubDir)
+        generateWithGenerators(resConfigList[resConfigN], generatorList, resSrcDir, cacheSubDir)
     }
 }
 
-function generateResourcesInModules(context, generatorMap) {
+function generateResourcesInModules(context, generatorList) {
     const modules = require("./modules.js");
 
     let cacheDir = path.join(context.projectRoot, "flappy_cache");
     if (!fs.existsSync(cacheDir))
         fs.mkdirSync(cacheDir);
 
-    generateResourcesInContext(context, generatorMap, cacheDir);
+    generateResourcesInContext(context, generatorList, cacheDir);
 
     // Iterate all modules in a project
     const allModulesContexts = modules.findAllModules(context);
     for (let i in allModulesContexts) {
         const moduleContext = allModulesContexts[i];
-        generateResourcesInContext(moduleContext, generatorMap, cacheDir);
-    }
-}
-
-function generateWithGenerators(config, generatorList, resSrcDir, cacheDir) {
-    for (const i in generatorList) {
-        const generator = generatorList[i];
-        if (generator.type == config.type)
-            genrator.generate(config, resSrcDir, cacheDir);
+        generateResourcesInContext(moduleContext, generatorList, cacheDir);
     }
 }
 
 function generateResources(params) {
     const context = utils.createContext(params, params.projectRoot, "default.json");
-    let generatorList = getMapOfGenerators(context);
+    let generatorList = getListOfGenerators(context);
     generateResourcesInModules(context, generatorList);
 }
 
