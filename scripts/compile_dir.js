f562d4d13dc81bcc47fdf36fe0ef19480a08902e
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-08-27 13:13:31 +0300

    Removed extra logging during project generation.

--- scripts/compile_dir.js
+++ scripts/compile_dir.js
@@ -5,7 +5,7 @@ function compileString(config, templateData) {
     const generate_js = require("./generate_js.js");
 
     const generationScript = generate_js.generate(templateData);
-    console.log("Script: \n" + generationScript);
+    //console.log("Script: \n" + generationScript);
     const compiledData = compile_js.compile(config, generationScript);
     return compiledData;
 }
@@ -14,13 +14,13 @@ function compileFile(config, filePath) {
     const fs = require("fs");
 
     const templateData = fs.readFileSync(filePath, 'utf8');
-    console.log("In: " + filePath);
+    //console.log("In: " + filePath);
     return compileString(config, templateData);
 }
 
 function saveFile(path, data) {
     var fsPath = require('fs-path');
-    console.log("Out: " + path);
+    //console.log("Out: " + path);
 
     fsPath.writeFileSync(path, data);
 }
