d30756e3ecce941a7c87021e988d51801ad72f80
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-08-27 17:24:24 +0300

    Fixed resource generation on build command.

--- scripts/build_project.js
+++ scripts/build_project.js
@@ -2,8 +2,9 @@
 
 function buildProject(params) {
     const utils = require("./utils.js");
+    const generateProject = require("./generate_project.js");
+    generateProject.generateProject(params);
     const context = utils.createContext(params, params.projectRoot, utils.defaultConfigFileName);
-    utils.requireGeneratorScript(params.generatorPath).generate(context);
     utils.requireGeneratorScript(params.generatorPath).build(context);
 }
 
