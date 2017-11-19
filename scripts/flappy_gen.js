46a94b720a3181c7eb9f7c10a2efeb648ce62dbb
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-11-19 17:33:55 +0300

    Moved call of resource generation from generation_project.js to flappy_gen.js to make possible calling of project generation without resource generation.

--- scripts/flappy_gen.js
+++ scripts/flappy_gen.js
@@ -3,6 +3,8 @@
 
 const utils = require("./utils.js");
 const generateProject = require("./generate_project.js");
+const generateResources = require("./generate_resources.js");
+const packResources = require("./pack_resources.js");
 
 const opt = require('node-getopt').create([
     ['o', 'output-dir=ARG', 'Output dir.'],
@@ -25,4 +27,7 @@ const templateName = opt.argv[0];
 const configOrder = opt.argv.slice(1);
 
 var params = utils.findParams(projectRoot, templateName, projectOutDir, configOrder, []);
+
+generateResources.generateResources(params);
+packResources.packResources(params);
 generateProject.generateProject(params);
