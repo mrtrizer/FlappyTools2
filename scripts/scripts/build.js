1411f387bf199ab0d4dc663a7f4c2e313e7923fc
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-12-02 13:06:09 +0300

    Fixed build script.

--- scripts/scripts/build.js
+++ scripts/scripts/build.js
@@ -6,10 +6,16 @@ function getHelp() {
 }
 
 function run(context, args) {
+    const utils = context.require("./utils.js");
     if (args.length < 1)
         throw new Error("At least template name expected");
+    const templateName = args[0];
     const genScript = context.requireFlappyScript("gen");
     genScript.run(context, args);
+    const generatorPath = utils.findTemplate(context.templateDirs, templateName);
+    const buildContext = utils.createBuildContext(context, generatorPath, "project_conf");
+    var projectGenerator = utils.requireGeneratorScript(generatorPath);
+    projectGenerator.build(buildContext);
 }
 
 module.exports.run = run;
