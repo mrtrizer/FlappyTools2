15f747af8ebe136572a5770f0de61bf8d11cbe08
Denis Zdorovtsov <mrtrizer@gmail.com>
2017-12-09 00:28:39 +0300

    Moved TimestampCache out of utils.js.

--- scripts/utils.js
+++ scripts/utils.js
@@ -321,34 +321,6 @@ function createBuildContext(moduleContext, generatorPath, configDirName) {
     return context;
 }
 
-// TODO: Move to separate file
-class TimestampCache {
-    constructor(context) {
-        const path = require('path');
-        const fse = require('fs-extra');
-        this.timestampCachePath = path.join(context.projectRoot, "flappy_cache/timestamps.json");
-        try {
-            this.timestamps = fse.readJsonSync(this.timestampCachePath);
-        } catch (e) {
-            this.timestamps = {};
-        }
-    }
-
-    isChanged(path) {
-        const fs = require('fs');
-        const fse = require('fs-extra');
-        const lastModifTime = Math.floor(fs.statSync(path).mtime);
-        if (!(path in this.timestamps) || this.timestamps[path] != lastModifTime) {
-            console.log(path + " - Changed : " + lastModifTime);
-            this.timestamps[path] = lastModifTime;
-            fse.outputJsonSync(this.timestampCachePath, this.timestamps, {spaces: 4});
-            return true;
-        }
-        return false;
-    }
-}
-
-module.exports.TimestampCache = TimestampCache;
 module.exports.defaultConfigFileName = "default.json";
 module.exports.absolutePath = absolutePath;
 module.exports.findProjectRoot = findProjectRoot;
